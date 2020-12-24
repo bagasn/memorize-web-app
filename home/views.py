ad43e1d1635d0db5efadf61c01d6e6f9eb5b4379
bagasn <bagasnasution@gmail.com>
2020-12-24 23:08:05 +0700

    Init commit 2

--- home/views.py
+++ home/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'login.html')
+    context = {
+        "page_title": "Login",
+    }
+    return render(request, 'login.html', context)
 
